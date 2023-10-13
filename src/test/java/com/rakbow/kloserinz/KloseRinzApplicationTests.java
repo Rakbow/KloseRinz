d96dd611e41ce9c8e0877b0e519925eade6f54ed
Rakbow <rakbow@gmail.com>
2023-10-13 21:50:54 +0800

    Initial commit

---
+++ src/test/java/com/rakbow/kloserinz/KloseRinzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakbow.kloserinz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KloseRinzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
