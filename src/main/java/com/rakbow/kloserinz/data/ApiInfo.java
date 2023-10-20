eaf385b8bc3d6bf6a446e5b3cdac909847fe9cff
Rakbow <rakbow@gmail.com>
2023-10-21 05:19:30 +0800

    新增统一字符串消息管理类

---
+++ src/main/java/com/rakbow/kloserinz/data/ApiInfo.java
@@ -0,0 +1,16 @@
+package com.rakbow.kloserinz.data;
+
+/**
+ * @Project_name: KloseRinz
+ * @Author: Rakbow
+ * @Create: 2023-10-21 3:38
+ * @Description:
+ */
+public class ApiInfo {
+
+    public static final String SKU_NAME_ZH_DEFICIENCY = "商品中文名称缺失";
+    public static final String SKU_NAME_EN_DEFICIENCY = "商品英文名称缺失";
+    public static final String SKU_CODE_DEFICIENCY = "商品编码缺失";
+    public static final String SKU_CATEGORY_DEFICIENCY = "商品分类缺失";
+
+}
